d2172f2a36a1d07d38e68cd614265231d02f769c
Idrek <iddarg@gmail.com>
2020-07-23 20:59:06 +0200

    New type aliases for `DeviantArt` namespace

---
+++ src/Aliases.fs
@@ -0,0 +1,8 @@
+namespace DeviantArt
+
+type AccessTokenResponse = Types.AccessTokenResponse
+type ContentType = HttpFs.Client.ContentType
+type DateTimeOffset = System.DateTimeOffset
+type FailureTokenResponse = Types.FailureTokenResponse
+type TRequest = HttpFs.Client.Request
+type TResponse = HttpFs.Client.Response
