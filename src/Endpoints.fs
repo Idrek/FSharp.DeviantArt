9baa439c8f3dec027c376e16ac1b5da034ddc946
Idrek <iddarg@gmail.com>
2020-07-23 21:55:52 +0200

    New `Endpoints.WithDefaults` member

--- src/Endpoints.fs
+++ src/Endpoints.fs
@@ -2,5 +2,11 @@ namespace DeviantArt
 
 type Endpoints = {
     CategoryTree: string
-}
+} with
+    static member WithDefaults () : Endpoints =
+        let prefix = "https://www.deviantart.com/api/v1/oauth2"
+        let browse = "browse"
+        {
+            CategoryTree = sprintf "%s/%s/categorytree" prefix browse
+        }
 
