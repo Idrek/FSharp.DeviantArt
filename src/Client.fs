f82c101921788e0ab6acaae79b28fbf6d4bde20c
Idrek <iddarg@gmail.com>
2020-07-23 22:04:30 +0200

    New `Client.BuildTokenResponseJob` member

--- src/Client.fs
+++ src/Client.fs
@@ -1,18 +1,19 @@
 ﻿namespace DeviantArt
 
+open FSharp.Json
+open Hopac
+
 // ---------------------------------
 // Module aliases
 // ---------------------------------
 
 module C = HttpFs.Client
 module MRequest = C.Request
+module S = Settings
 
 // ---------------------------------
 // Type aliases
 // ---------------------------------
-
-type ContentType = HttpFs.Client.ContentType
-type TRequest = C.Request
 
 // ---------------------------------
 // Functions
@@ -34,4 +35,39 @@ type Client = {
                 C.FormData.NameValue ("grant_type", "client_credentials")])
         request
 
+    static member BuildTokenResponseJob
+            (choice: Choice<TResponse, exn>)
+            (readBodyAsString: TResponse -> Job<string>)
+            : Job<Result<AccessTokenResponse, FailureTokenResponse>> =
+        let buildSuccessResponse (json: string) : AccessTokenResponse =
+            json |> Json.deserializeEx<AccessTokenResponse> S.jsonConfig
+        let buildFailureResponse (json: string) : FailureTokenResponse =
+            json |> Json.deserializeEx<FailureTokenResponse> S.jsonConfig
+        let buildClientErrorResponse (description: string) : FailureTokenResponse =
+            {
+                Error = "client_error"
+                ErrorDescription = description
+                ErrorDetails = None
+                ErrorCode = None
+            }                   
+        job {
+            try            
+                match choice with
+                | Choice2Of2 exn -> 
+                    let error = Error <| buildClientErrorResponse exn.Message
+                    return error
+                | Choice1Of2 response ->  
+                    let! bodyJson = readBodyAsString response
+                    match response.statusCode with
+                    | 200 ->
+                        let successResponse = Ok <| buildSuccessResponse bodyJson
+                        return successResponse
+                    | _ -> 
+                        let failureResponse = Error <| buildFailureResponse bodyJson
+                        return failureResponse
+            with
+            | e ->
+                let error = Error <| buildClientErrorResponse e.Message
+                return error                
+        }      
 
