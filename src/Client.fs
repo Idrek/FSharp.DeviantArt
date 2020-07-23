1c32b6f0cd3c84fd10517122e8753edf7413b358
Idrek <iddarg@gmail.com>
2020-07-23 17:59:56 +0200

    New `Client.BuildTokenRequest` member

--- src/Client.fs
+++ src/Client.fs
@@ -4,9 +4,15 @@
 // Module aliases
 // ---------------------------------
 
+module C = HttpFs.Client
+module MRequest = C.Request
+
 // ---------------------------------
 // Type aliases
 // ---------------------------------
+
+type ContentType = HttpFs.Client.ContentType
+type TRequest = C.Request
 
 // ---------------------------------
 // Functions
@@ -15,6 +21,17 @@
 type Client = {
     AccessToken: string
     TokenType: string
-}
+} with
+    static member BuildTokenRequest (clientId: int) (clientSecret: string) (tokenUrl: string) : TRequest =
+        let request = 
+            MRequest.createUrl (C.HttpMethod.Post) tokenUrl
+            |> MRequest.setHeader (
+                ContentType.create ("application", "x-www-form-urlencoded")
+                |> C.RequestHeader.ContentType)
+            |> MRequest.body (C.RequestBody.BodyForm [
+                C.FormData.NameValue ("client_id", string clientId)
+                C.FormData.NameValue ("client_secret", clientSecret)
+                C.FormData.NameValue ("grant_type", "client_credentials")])
+        request
 
 
