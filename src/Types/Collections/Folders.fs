8da1ee64e89fc42616d0650e1bfaab51a79d4e20
Idrek <iddarg@gmail.com>
2020-07-24 22:14:45 +0200

    New `Folders` module

---
+++ src/Types/Collections/Folders.fs
@@ -0,0 +1,54 @@
+module DeviantArt.Types.Collections.Folders
+
+// ---------------------------------
+// Module aliases
+// ---------------------------------
+
+module D = DeviantArt.Types.Deviation
+module R = DeviantArt.Rules
+module T = Validator.Types
+module V = Validator.Api
+
+// ---------------------------------
+// Type aliases
+// ---------------------------------
+
+type Guid = System.Guid
+
+// ---------------------------------
+// Types
+// ---------------------------------
+
+[<CLIMutable>]
+type Parameters = {
+    Username: Option<string>
+    CalculateSize: Option<bool>
+    ExtPreload: Option<bool>
+    Offset: Option<int>
+    Limit: Option<int>
+    MatureContent: bool
+} with
+    member this.Validate () : Result<Parameters, Set<T.Invalid>> =
+        let v = V.validator<Parameters>() {
+            validateOptional "Offset" (fun this -> this.Offset) [
+                R.inRange (0, 50000)
+            ]
+            validateOptional "Limit" (fun this -> this.Limit) [
+                R.inRange (1, 50)
+            ]
+        }
+        v this |> Result.map (fun _ -> this)
+
+type Folder = {
+    Folderid: Guid
+    Name: string
+    Size: Option<int>
+    Deviations: Option<array<D.Deviation>>
+}
+
+type Response = {
+    HasMore: bool
+    NextOffset: Option<int>
+    Results: Option<array<Folder>>
+}
+
