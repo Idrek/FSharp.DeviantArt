833053d500f4df3563a26320ca9652f456124ca9
Idrek <iddarg@gmail.com>
2020-07-24 16:05:02 +0200

    New `MoreLikeThisPreview` module with `Parameters` and `Response` records

---
+++ src/Types/Browse/MoreLikeThisPreview.fs
@@ -0,0 +1,30 @@
+module DeviantArt.Types.Browse.MoreLikeThisPreview
+
+// ---------------------------------
+// Module aliases
+// ---------------------------------
+
+module D = DeviantArt.Types.Deviation
+
+// ---------------------------------
+// Type aliases
+// ---------------------------------
+
+type Guid = System.Guid
+
+// ---------------------------------
+// Functions
+// ---------------------------------
+
+[<CLIMutable>]
+type Parameters = {
+    Seed: Guid
+    MatureContent: bool
+}
+
+type Response = {
+    Seed: Guid
+    Author: D.User
+    MoreFromArtist: array<D.Deviation>
+    MoreFromDa: array<D.Deviation>
+}
