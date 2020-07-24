f3fd42401d8481e21c2e64796f57ecaf633f9976
Idrek <iddarg@gmail.com>
2020-07-24 15:19:52 +0200

    Add new `Endpoints.HotDeviations` field

--- src/Endpoints.fs
+++ src/Endpoints.fs
@@ -3,6 +3,7 @@ namespace DeviantArt
 type Endpoints = {
     CategoryTree: string
     DailyDeviations: string
+    HotDeviations: string
 } with
     static member WithDefaults () : Endpoints =
         let prefix = "https://www.deviantart.com/api/v1/oauth2"
@@ -10,5 +11,6 @@ type Endpoints = {
         {
             CategoryTree = sprintf "%s/%s/categorytree" prefix browse
             DailyDeviations = sprintf "%s/%s/dailydeviations" prefix browse
+            HotDeviations = sprintf "%s/%s/hot" prefix browse
         }
 
