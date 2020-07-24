28786c91c4ac7cec91d848f43c29d7d3f50413aa
Idrek <iddarg@gmail.com>
2020-07-24 11:54:48 +0200

    New `Endpoints.DailyDeviations` field

--- src/Endpoints.fs
+++ src/Endpoints.fs
@@ -2,11 +2,13 @@ namespace DeviantArt
 
 type Endpoints = {
     CategoryTree: string
+    DailyDeviations: string
 } with
     static member WithDefaults () : Endpoints =
         let prefix = "https://www.deviantart.com/api/v1/oauth2"
         let browse = "browse"
         {
             CategoryTree = sprintf "%s/%s/categorytree" prefix browse
+            DailyDeviations = sprintf "%s/%s/dailydeviations" prefix browse
         }
 
