8f5f78c3e6fb6fc6e1150fb6dcc70fc06504bd10
Idrek <iddarg@gmail.com>
2020-07-24 16:18:35 +0200

    New `Endpoints.Popular` field

--- src/Endpoints.fs
+++ src/Endpoints.fs
@@ -7,6 +7,7 @@ type Endpoints = {
     MoreLikeThis: string
     MoreLikeThisPreview: string
     Newest: string
+    Popular: string
 } with
     static member WithDefaults () : Endpoints =
         let prefix = "https://www.deviantart.com/api/v1/oauth2"
@@ -18,5 +19,6 @@ type Endpoints = {
             MoreLikeThis = sprintf "%s/%s/morelikethis" prefix browse
             MoreLikeThisPreview = sprintf "%s/%s/morelikethis/preview" prefix browse
             Newest = sprintf "%s/%s/newest" prefix browse
+            Popular = sprintf "%s/%s/popular" prefix browse
         }
 
