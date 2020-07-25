f229e430241f7cd8483c936977d5711200238a65
Idrek <iddarg@gmail.com>
2020-07-25 18:42:14 +0200

    New `Data.Countries` module

---
+++ src/Types/Data/Countries.fs
@@ -0,0 +1,29 @@
+module DeviantArt.Types.Data.Countries
+
+// ---------------------------------
+// Module aliases
+// ---------------------------------
+
+module D = DeviantArt.Types.Deviation
+module R = DeviantArt.Rules
+module T = Validator.Types
+module V = Validator.Api
+
+// ---------------------------------
+// Functions
+// ---------------------------------
+
+[<CLIMutable>]
+type Parameters = {
+    MatureContent: bool
+}
+
+type Country = {
+    Countryid: int
+    Name: string
+}
+
+type Response = {
+    Results: array<Country>
+}
+
