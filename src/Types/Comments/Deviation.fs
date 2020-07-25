61a19b434b943736b6b6de06103f9253ab616438
Idrek <iddarg@gmail.com>
2020-07-25 12:03:10 +0200

    New `Comments.Deviation` module

---
+++ src/Types/Comments/Deviation.fs
@@ -0,0 +1,53 @@
+module DeviantArt.Types.Comments.Deviation
+
+// ---------------------------------
+// Module aliases
+// ---------------------------------
+
+module Common = DeviantArt.Types.Common
+module D = DeviantArt.Types.Deviation
+module R = DeviantArt.Rules
+module T = Validator.Types
+module V = Validator.Api
+
+// ---------------------------------
+// Type aliases
+// ---------------------------------
+
+type Guid = System.Guid
+
+// ---------------------------------
+// Types
+// ---------------------------------
+
+[<CLIMutable>]
+type Parameters = {
+    DeviationId: Guid
+    CommentId: Option<Guid>
+    MaxDepth: Option<int>
+    Offset: Option<int>
+    Limit: Option<int>
+    MatureContent: bool
+} with
+    member this.Validate () : Result<Parameters, Set<T.Invalid>> =
+        let v = V.validator<Parameters>() {
+            validateOptional "Offset" (fun this -> this.Offset) [
+                R.inRange (-10000, 10000)
+            ]
+            validateOptional "Limit" (fun this -> this.Limit) [
+                R.inRange (1, 50)
+            ]
+            validateOptional "Max depth" (fun this -> this.MaxDepth) [
+                R.inRange (0, 5)
+            ]
+        }
+        v this |> Result.map (fun _ -> this)
+
+type Response = {
+    HasMore: bool
+    NextOffset: Option<int>
+    HasLess: bool
+    PrevOffset: Option<int>
+    Total: Option<int>
+    Thread: array<Common.ThreadComment>
+}
