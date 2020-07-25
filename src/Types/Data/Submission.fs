94eb3ac34d14eaeb4a14cf77b54667b061f47d77
Idrek <iddarg@gmail.com>
2020-07-25 19:02:22 +0200

    New `Data.Submission` module

---
+++ src/Types/Data/Submission.fs
@@ -0,0 +1,19 @@
+module DeviantArt.Types.Data.Submission
+
+// ---------------------------------
+// Module aliases
+// ---------------------------------
+
+// ---------------------------------
+// Functions
+// ---------------------------------
+
+[<CLIMutable>]
+type Parameters = {
+    MatureContent: bool
+}
+
+type Response = {
+    Text: string
+}
+
