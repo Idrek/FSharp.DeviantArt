7e4d7ee9c9d48c7a07f65c106cebf75b8fad99cb
Idrek <iddarg@gmail.com>
2020-07-26 21:57:12 +0200

    New `Gallery.All` module

---
+++ src/Types/Gallery/All.fs
@@ -0,0 +1,57 @@
+module DeviantArt.Types.Gallery.All
+
+// ---------------------------------
+// Module aliases
+// ---------------------------------
+
+module R = DeviantArt.Rules
+module T = Validator.Types
+module V = Validator.Api
+
+// ---------------------------------
+// Type aliases
+// ---------------------------------
+
+type Deviation = DeviantArt.Types.Common.Deviation.Deviation
+
+// ---------------------------------
+// Types
+// ---------------------------------
+
+[<CLIMutable>]
+type Parameters = {
+    Username: Option<string>
+    Offset: Option<int>
+    Limit: Option<int>
+    MatureContent: bool
+} with
+    member this.Validate () : Result<Parameters, Set<T.Invalid>> =
+        let v = V.validator<Parameters>() {
+            validateOptional "Offset" (fun this -> this.Offset) [
+                R.inRange (0, 50000)
+            ]
+            validateOptional "Limit" (fun this -> this.Limit) [
+                R.inRange (1, 24)
+            ]
+        }
+        v this |> Result.map (fun _ -> this)
+
+    static member Initialize 
+            (
+                ?username: Option<string>,
+                ?matureContent: bool,
+                ?offset: Option<int>, 
+                ?limit: Option<int>
+            ) : Parameters =
+        {
+            Username = defaultArg username None
+            MatureContent = defaultArg matureContent false
+            Offset = defaultArg offset None
+            Limit = defaultArg limit None
+        }
+
+type Response = {
+    HasMore: bool
+    NextOffset: Option<int>
+    Results: array<Deviation>
+}
