c0ac8871d96f23c1627230e83531a05434c6c41f
Idrek <iddarg@gmail.com>
2020-07-26 21:14:34 +0200

    New `String` alias

--- src/Aliases.fs
+++ src/Aliases.fs
@@ -4,5 +4,6 @@ type AccessTokenResponse = Types.AccessTokenResponse
 type ContentType = HttpFs.Client.ContentType
 type DateTimeOffset = System.DateTimeOffset
 type FailureTokenResponse = Types.FailureTokenResponse
+type String = System.String
 type TRequest = HttpFs.Client.Request
 type TResponse = HttpFs.Client.Response
