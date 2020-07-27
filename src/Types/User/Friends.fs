6dfc32c1a2236dd667fb1066561d9beed1d65c88
Idrek <iddarg@gmail.com>
2020-07-27 21:09:45 +0200

    New `User.Friends` module

---
+++ src/Types/User/Friends.fs
@@ -0,0 +1,78 @@
+module DeviantArt.Types.User.Friends
+
+// ---------------------------------
+// Module aliases
+// ---------------------------------
+
+module R = DeviantArt.Rules
+module T = Validator.Types
+module V = Validator.Api
+
+// ---------------------------------
+// Type aliases
+// ---------------------------------
+
+type Deviation = DeviantArt.Types.Common.Deviation.Deviation
+type Guid = System.Guid
+type User = DeviantArt.Types.Common.Deviation.User
+
+// ---------------------------------
+// Types
+// ---------------------------------
+
+[<CLIMutable>]
+type Parameters = {
+    Username: string
+    Offset: Option<int>
+    Limit: Option<int>
+    MatureContent: bool
+} with
+    member this.Validate () : Result<Parameters, Set<T.Invalid>> =
+        let v = V.validator<Parameters>() {
+            validateOptional "Offset" (fun this -> this.Offset) [
+                R.inRange (0, 50000)
+            ]
+            validateOptional "Limit" (fun this -> this.Limit) [
+                R.inRange (1, 50)
+            ]
+        }
+        v this |> Result.map (fun _ -> this)
+
+    static member Initialize 
+            (
+                username: string,
+                ?matureContent: bool,
+                ?offset: Option<int>, 
+                ?limit: Option<int>
+            ) : Parameters =
+        {
+            Username = username
+            MatureContent = defaultArg matureContent false
+            Offset = defaultArg offset None
+            Limit = defaultArg limit None
+        }
+
+type Watch = {
+    Friend: bool
+    Deviations: bool
+    Journals: bool
+    ForumThreads: bool
+    Critiques: bool
+    Scraps: bool
+    Activity: bool
+    Collections: bool
+}
+
+type Friend = {
+    User: User
+    IsWatching: bool
+    WatchesYou: bool
+    Lastvisit: Option<string>
+    Watch: Watch
+}
+
+type Response = {
+    HasMore: bool
+    NextOffset: Option<int>
+    Results: array<Friend>
+}
