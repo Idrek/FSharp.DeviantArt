4ca4840446e6d90971c9e4b91168229ff56ed165
Idrek <iddarg@gmail.com>
2020-07-28 22:37:25 +0200

    New `Initialize` member to `Browse.UserJournals` module

--- src/Types/Browse/UserJournals.fs
+++ src/Types/Browse/UserJournals.fs
@@ -35,6 +35,22 @@ type Parameters = {
         }
         v this |> Result.map (fun _ -> this)
 
+    static member Initialize 
+            (
+                username: string,
+                ?featured: Option<bool>,
+                ?matureContent: bool,
+                ?offset: Option<int>,
+                ?limit: Option<int>
+            ) : Parameters =
+        {
+            Username = username
+            Featured = defaultArg featured None
+            MatureContent = defaultArg matureContent false
+            Offset = defaultArg offset None
+            Limit = defaultArg limit None
+        }
+
 type Response = {
     HasMore: bool
     NextOffset: Option<int>
