f648fff5ad487d335acc9799c2711c57666ba026
Idrek <iddarg@gmail.com>
2020-07-28 11:53:06 +0200

    New `User.Statuses` module

---
+++ src/Types/User/Statuses.fs
@@ -0,0 +1,61 @@
+module DeviantArt.Types.User.Statuses
+
+// ---------------------------------
+// Module aliases
+// ---------------------------------
+
+module R = DeviantArt.Rules
+module T = Validator.Types
+module V = Validator.Api
+
+// ---------------------------------
+// Type aliases
+// ---------------------------------
+
+// TODO: Move `Status` to common or shared.
+type Status = DeviantArt.Types.Comments.Siblings.Status
+
+// ---------------------------------
+// Types
+// ---------------------------------
+
+[<CLIMutable>]
+type Parameters = {
+    Username: string
+    Offset: Option<int>
+    Limit: Option<int>
+    MatureContent: bool
+} with
+    member this.Validate () : Result<Parameters, Set<T.Invalid>> =
+        let v = V.validator<Parameters>() {
+            validateOptional "Offset" (fun this -> this.Offset) [
+                R.inRange (0, 50000)
+            ]
+            validateOptional "Limit" (fun this -> this.Limit) [
+                R.inRange (1, 50)
+            ]
+            validateWith "Username" (fun this -> this.Username) [
+                R.isNotEmptyString
+            ]
+        }
+        v this |> Result.map (fun _ -> this)
+
+    static member Initialize 
+            (
+                username: string,
+                ?matureContent: bool,
+                ?offset: Option<int>, 
+                ?limit: Option<int>
+            ) : Parameters =
+        {
+            Username = username
+            MatureContent = defaultArg matureContent false
+            Offset = defaultArg offset None
+            Limit = defaultArg limit None
+        }
+
+type Response = {
+    HasMore: bool
+    NextOffset: Option<int>
+    Results: array<Status>
+}
