69de2c40ececdf490351347e50ef9024c53ae129
Idrek <iddarg@gmail.com>
2020-07-28 13:51:35 +0200

    Add `Initialize` member to `Browse.Daily` module

--- src/Types/Browse/Daily.fs
+++ src/Types/Browse/Daily.fs
@@ -14,7 +14,16 @@ module D = DeviantArt.Types.Common.Deviation
 type Parameters = {
     Date: Option<string>
     MatureContent: bool
-}
+} with
+    static member Initialize 
+            (
+                ?matureContent: bool, 
+                ?date: Option<string>
+            ) : Parameters =
+        {
+            Date = defaultArg date None
+            MatureContent = defaultArg matureContent false
+        }
 
 type Response = {
     Results: array<D.Deviation>
