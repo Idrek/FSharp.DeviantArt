5dc2592f6340a53c127e99fb7ee119f5b8917ad7
Idrek <iddarg@gmail.com>
2020-07-29 11:33:04 +0200

    New `Initialize` member to `Data.Submission` module

--- src/Types/Data/Submission.fs
+++ src/Types/Data/Submission.fs
@@ -11,7 +11,14 @@ module DeviantArt.Types.Data.Submission
 [<CLIMutable>]
 type Parameters = {
     MatureContent: bool
-}
+} with
+    static member Initialize 
+            (
+                ?matureContent: bool
+            ) : Parameters =
+        {
+            MatureContent = defaultArg matureContent false
+        }
 
 type Response = {
     Text: string
